e19d210890671312698cdb10505ab65d9abfe943
mnadeem <coolmind182006@gmail.com>
2015-01-26 22:56:44 +0530

    refactored

--- app/src/Driver.ts
+++ app/src/Driver.ts
@@ -9,15 +9,14 @@ module sample {
 		}
 
 		draw() {	
-			var canvas = <HTMLCanvasElement>window.document.getElementById("drawingCanvas");
-			var ctx = canvas.getContext("2d");
+			var canvas:HTMLCanvasElement = <HTMLCanvasElement>window.document.getElementById("drawingCanvas");
+			var ctx:CanvasRenderingContext2D = canvas.getContext("2d");
 
-			var line = new Line(new Point(100, 150), new Point(450, 50));
+			var line:Line = new Line(new Point(100, 150), new Point(450, 50));
 			line.draw(ctx);
 			
 			var circle = new Circle(new Point(100, 70), 50);
 			circle.draw(ctx);
-		
 		}
 	}
 }
